0b4aa899c55de3e973af08f60ba26f0f5a1739d3
Alektas <kriowolf1@mail.ru>
2018-10-17 09:11:26 +0300

    Add base listview adapter and inheritance from it basket's and showcase' adapters.

--- app/src/main/java/alektas/pocketbasket/view/BasketAdapter.java
+++ app/src/main/java/alektas/pocketbasket/view/BasketAdapter.java
@@ -6,32 +6,25 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.Resources;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.List;
 
 import alektas.pocketbasket.R;
 import alektas.pocketbasket.db.entity.Item;
 import alektas.pocketbasket.viewmodel.ItemsViewModel;
 
-public class BasketAdapter extends BaseAdapter {
+public class BasketAdapter extends BaseItemAdapter {
     private static final String TAG = "BasketAdapter";
     private final float DEL_EDGE = 0.6f;
     private final float TAP_PADDING;
     private final float CHECKABLE_ZONE;
     private Context mContext;
     private ItemsViewModel mModel;
-    private List<Item> mItems;
 
     BasketAdapter(Context context, ItemsViewModel model) {
+        super(context);
         mContext = context;
         mModel = model;
 
@@ -41,123 +34,37 @@ public class BasketAdapter extends BaseAdapter {
         TAP_PADDING = CHECKABLE_ZONE;
     }
 
-    static class ViewHolder {
-        final View mItemView;
-        final ImageView mImage;
-        final TextView mIconText;
-        final ImageView mCheckImage;
-        final TextView mName;
-
-        ViewHolder(View view) {
-            mItemView = view;
-            mImage = mItemView.findViewById(R.id.item_image);
-            mIconText = mItemView.findViewById(R.id.info_text);
-            mCheckImage = mItemView.findViewById(R.id.check_image);
-            mName = mItemView.findViewById(R.id.item_name);
-        }
-    }
-
-    public int getCount() {
-        if (mItems != null) return mItems.size();
-        return 0;
-    }
-
-    public Object getItem(int position) {
-        return mItems.get(position);
-    }
-
-    public long getItemId(int position) {
-        return position;
-    }
-
     @SuppressLint("ClickableViewAccessibility")
+    @Override
     public View getView(int position, View convertView, final ViewGroup parent) {
-        View itemView = convertView;
-
-        ViewHolder viewHolder;
-        if (itemView == null) {
-            itemView = initView(parent);
-            viewHolder = new ViewHolder(itemView);
-            itemView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        final Item item;
-        if (mItems != null) { item = mItems.get(position); }
-        else item = new Item("Item");
-
-        bindViewWithData(viewHolder, item);
-
+        View itemView = super.getView(position, convertView, parent);
         itemView.setOnTouchListener((view, motionEvent) -> {
-            parent.setOnTouchListener(getSwipeListener(view, item.getName()));
+            parent.setOnTouchListener(
+                    getSwipeListener(view, ((Item)getItem(position)).getName()));
             return false; // need to be false to allow sliding at the item view zone
         });
 
         return itemView;
     }
 
-    public void setItems(List<Item> items) {
-        mItems = items;
-        notifyDataSetChanged();
-        Log.d(TAG, "setItems: basket is notified");
-    }
-
-    // inflate item View from resources
-    // or get ViewHolder from saves if exist
-    private View initView(ViewGroup parent) {
-        LayoutInflater inflater =
-                (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        assert inflater != null;
-        return inflater.inflate(R.layout.item_view, parent, false);
-    }
-
-    private void bindViewWithData(ViewHolder viewHolder, Item item) {
-        setItemText(viewHolder, item);
-        setItemIcon(viewHolder, item);
-        setChooseIcon(viewHolder, item);
-    }
-
     // hide item name in showcase mode and show in basket mode in "Basket"
-    private void setItemText(ViewHolder viewHolder, Item item) {
+    @Override
+    void setItemText(ViewHolder viewHolder, Item item) {
         if (mModel.isBasketNamesShow()) {
             viewHolder.mName.setText(getItemName(item));
         }
         else viewHolder.mName.setText("");
     }
 
-    // set item icon (or name instead)
-    private void setItemIcon(ViewHolder viewHolder, Item item) {
-        if (item.getImgRes() > 0) {
-            viewHolder.mImage.setImageResource(item.getImgRes());
-            viewHolder.mIconText.setText("");
-        } else {
-            viewHolder.mImage.setImageResource(0);
-            viewHolder.mIconText.setText(getItemName(item));
-        }
-    }
-
     // add check image to icon of item in basket if item is checked
-    private void setChooseIcon(ViewHolder viewHolder, Item item) {
+    @Override
+    void setChooseIcon(ViewHolder viewHolder, Item item) {
         if (item.isChecked()) {
             viewHolder.mCheckImage.setImageResource(R.drawable.ic_checked);
         }
         else {
             viewHolder.mCheckImage.setImageResource(0);
         }
-    }
-
-    // get item name from resources or from key field if res is absent
-    private String getItemName(Item item) {
-        String itemName;
-        int nameRes = item.getNameRes();
-        try {
-            itemName = mContext.getString(nameRes);
-        }
-        catch (Resources.NotFoundException e) {
-            itemName = item.getName();
-        }
-        return itemName;
     }
 
     // ListView listener for processing items sliding and check
--- app/src/main/java/alektas/pocketbasket/view/ShowcaseAdapter.java
+++ app/src/main/java/alektas/pocketbasket/view/ShowcaseAdapter.java
@@ -1,15 +1,9 @@
 package alektas.pocketbasket.view;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.List;
 
@@ -17,72 +11,27 @@ import alektas.pocketbasket.R;
 import alektas.pocketbasket.db.entity.Item;
 import alektas.pocketbasket.viewmodel.ItemsViewModel;
 
-public class ShowcaseAdapter extends BaseAdapter {
+public class ShowcaseAdapter extends BaseItemAdapter {
     private static final String TAG = "ShowcaseAdapter";
     private MainActivity mActivity;
-    private Context mContext;
     private ItemsViewModel mModel;
-    private List<Item> mItems;
     private List<Item> mDelItems;
 
     ShowcaseAdapter(MainActivity activity, ItemsViewModel model) {
+        super(activity.getApplicationContext());
         mActivity = activity;
-        mContext = activity.getApplicationContext();
         mModel = model;
         mDelItems = model.getDelItems();
     }
 
-    static class ViewHolder {
-        final View mItemView;
-        final ImageView mImage;
-        final TextView mIconText;
-        final ImageView mCheckImage;
-        final ImageView mDelImage;
-        final TextView mName;
-
-        ViewHolder(View view) {
-            mItemView = view;
-            mImage = mItemView.findViewById(R.id.item_image);
-            mIconText = mItemView.findViewById(R.id.info_text);
-            mCheckImage = mItemView.findViewById(R.id.check_image);
-            mName = mItemView.findViewById(R.id.item_name);
-            mDelImage = mItemView.findViewById(R.id.del_image);
-        }
-    }
-
-    public int getCount() {
-        if (mItems != null) return mItems.size();
-        return 0;
-    }
-
-    public Object getItem(int position) {
-        return mItems.get(position);
-    }
-
-    public long getItemId(int position) {
-        return position;
-    }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View itemView = convertView;
-
-        ViewHolder viewHolder;
-        if (itemView == null) {
-            itemView = initView(parent);
-            viewHolder = new ViewHolder(itemView);
-            itemView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        final Item item;
-        if (mItems != null) { item = mItems.get(position); }
-        else item = new Item("Item");
-
+        View itemView = super.getView(position, convertView, parent);
+        ViewHolder viewHolder = (ViewHolder) itemView.getTag();
         viewHolder.mName.setTextColor(Color.WHITE);
 
-        bindViewWithData(viewHolder, item);
+        Item item = (Item) getItem(position);
+        setDelIcon(viewHolder, item);
 
         viewHolder.mItemView.setOnLongClickListener(view -> {
             enableDelMode();
@@ -106,49 +55,18 @@ public class ShowcaseAdapter extends BaseAdapter {
         return itemView;
     }
 
-    public void setItems(List<Item> items) {
-        mItems = items;
-        notifyDataSetChanged();
-        Log.d(TAG, "setItems: showcase is notified");
-    }
-
-    // inflate item View from resources
-    // or get ViewHolder from saves if exist
-    private View initView(ViewGroup parent) {
-        LayoutInflater inflater =
-                (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        assert inflater != null;
-        return inflater.inflate(R.layout.item_view, parent, false);
-    }
-
-    private void bindViewWithData(ViewHolder viewHolder, Item item) {
-        setItemText(viewHolder, item);
-        setItemIcon(viewHolder, item);
-        setChooseIcon(viewHolder, item);
-        setDelIcon(viewHolder, item);
-    }
-
     // show item name in showcase mode and hide in basket mode in "Showcase"
-    private void setItemText(ViewHolder viewHolder, Item item) {
+    @Override
+    void setItemText(ViewHolder viewHolder, Item item) {
         if (mModel.isShowcaseNamesShow()) {
             viewHolder.mName.setText(getItemName(item));
         }
         else viewHolder.mName.setText("");
     }
 
-    // set item icon (or name instead)
-    private void setItemIcon(ViewHolder viewHolder, Item item) {
-        if (item.getImgRes() > 0) {
-            viewHolder.mImage.setImageResource(item.getImgRes());
-            viewHolder.mIconText.setText("");
-        } else {
-            viewHolder.mImage.setImageResource(0);
-            viewHolder.mIconText.setText(getItemName(item));
-        }
-    }
-
     // add choose image to icon of item in showcase if item is present in basket
-    private void setChooseIcon(ViewHolder viewHolder, Item item) {
+    @Override
+    void setChooseIcon(ViewHolder viewHolder, Item item) {
         if (item.isInBasket()) {
             viewHolder.mCheckImage.setImageResource(R.drawable.ic_choosed);
         }
@@ -164,19 +82,6 @@ public class ShowcaseAdapter extends BaseAdapter {
         } else {
             viewHolder.mDelImage.setImageResource(0);
         }
-    }
-
-    // get item name from resources or from key field if res is absent
-    private String getItemName(Item item) {
-        String itemName;
-        int nameRes = item.getNameRes();
-        try {
-            itemName = mContext.getString(nameRes);
-        }
-        catch (Resources.NotFoundException e) {
-            itemName = item.getName();
-        }
-        return itemName;
     }
 
     public void deleteItems() {
