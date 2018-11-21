0019361c373fcbccaa37e8e2a0a0e938d437d08e
Alektas <kriowolf1@mail.ru>
2018-11-21 08:55:41 +0300

    Add process time measure to Utils

--- app/src/main/java/alektas/pocketbasket/Utils.java
+++ app/src/main/java/alektas/pocketbasket/Utils.java
@@ -1,11 +1,25 @@
 package alektas.pocketbasket;
 
 import android.content.res.Resources;
+import android.util.Log;
 
 public class Utils {
     private static Resources mResources = App.getComponent().context().getResources();
 
     public static String getString(int stringResId) {
         return mResources.getString(stringResId);
+    }
+
+    public static long measureProcessTime(Measurable process) {
+        long start = System.nanoTime();
+        process.run();
+        long end = System.nanoTime();
+        long time = end - start;
+        Log.d("ProcessTime", "measureProcessTime: " + time + " ns");
+        return time;
+    }
+
+    public interface Measurable {
+        void run();
     }
 }
