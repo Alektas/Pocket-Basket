cd10efe663e1e3efec890be91c0292735709b4f6
Alektas <kriowolf1@mail.ru>
2018-12-09 18:40:35 +0300

    Additions: add new items
    Add water, kefir, yogurt, condensed milk, flour, pizza, kiwi, plums,
    tangerines, wheat, millet, barley groats, corn grits, frozen vegetables.

--- app/src/main/java/alektas/pocketbasket/model/ItemGenerator.java
+++ app/src/main/java/alektas/pocketbasket/model/ItemGenerator.java
@@ -27,11 +27,14 @@ public class ItemGenerator {
         addItem(sItems, R.string.wine, 0, R.string.drink);
         addItem(sItems, R.string.vodka, 0, R.string.drink);
         addItem(sItems, R.string.chicory, 0, R.string.drink);
+        addItem(sItems, R.string.water, 0, R.string.drink);
 
         addItem(sItems, R.string.milk, R.drawable.ic_milk, R.string.milky);
         addItem(sItems, R.string.curd, 0, R.string.milky);
         addItem(sItems, R.string.sour_cream, 0, R.string.milky);
         addItem(sItems, R.string.cheese, R.drawable.ic_cheese, R.string.milky);
+        addItem(sItems, R.string.kefir, 0, R.string.milky);
+        addItem(sItems, R.string.yogurt, 0, R.string.milky);
 
         addItem(sItems, R.string.ice_cream, R.drawable.ice_cream, R.string.sweets);
         addItem(sItems, R.string.cookies, R.drawable.ic_cookies, R.string.sweets);
@@ -41,16 +44,22 @@ public class ItemGenerator {
         addItem(sItems, R.string.waffles, 0, R.string.sweets);
         addItem(sItems, R.string.candies, R.drawable.candies, R.string.sweets);
         addItem(sItems, R.string.sugar, 0, R.string.sweets);
+        addItem(sItems, R.string.condensed_milk, 0, R.string.sweets);
 
         addItem(sItems, R.string.brown_bread, R.drawable.bread, R.string.floury);
         addItem(sItems, R.string.white_bread, R.drawable.ic_white_bread, R.string.floury);
         addItem(sItems, R.string.pasta, R.drawable.ic_pasta, R.string.floury);
+        addItem(sItems, R.string.flour, 0, R.string.floury);
+        addItem(sItems, R.string.pizza, 0, R.string.floury);
 
         addItem(sItems, R.string.lemon, R.drawable.ic_lemon, R.string.fruit);
         addItem(sItems, R.string.apple, R.drawable.ic_apple, R.string.fruit);
         addItem(sItems, R.string.banana, R.drawable.ic_banana, R.string.fruit);
         addItem(sItems, R.string.orange, 0, R.string.fruit);
         addItem(sItems, R.string.grapes, R.drawable.ic_grapes, R.string.fruit);
+        addItem(sItems, R.string.kiwi, 0, R.string.fruit);
+        addItem(sItems, R.string.plums, 0, R.string.fruit);
+        addItem(sItems, R.string.tangerines, 0, R.string.fruit);
 
         addItem(sItems, R.string.potatoes, 0, R.string.vegetable);
         addItem(sItems, R.string.carrot, R.drawable.ic_carrot, R.string.vegetable);
@@ -66,6 +75,10 @@ public class ItemGenerator {
         addItem(sItems, R.string.oatmeal, 0, R.string.groats);
         addItem(sItems, R.string.buckwheat, 0, R.string.groats);
         addItem(sItems, R.string.peas, 0, R.string.groats);
+        addItem(sItems, R.string.wheat_groats, 0, R.string.groats);
+        addItem(sItems, R.string.millet_groats, 0, R.string.groats);
+        addItem(sItems, R.string.corn_grits, 0, R.string.groats);
+        addItem(sItems, R.string.barley_groats, 0, R.string.groats);
 
         addItem(sItems, R.string.pork, R.drawable.meat, R.string.meat);
         addItem(sItems, R.string.chicken, R.drawable.chicken, R.string.meat);
@@ -92,6 +105,7 @@ public class ItemGenerator {
         addItem(sItems, R.string.dumplings, 0, R.string.semis);
         addItem(sItems, R.string.cutlets, 0, R.string.semis);
         addItem(sItems, R.string.canned_food, 0, R.string.semis);
+        addItem(sItems, R.string.frozen_vegetables, 0, R.string.semis);
 
         addItem(sItems, R.string.mushrooms, R.drawable.ic_mushroom, R.string.other);
         addItem(sItems, R.string.egg, R.drawable.egg, R.string.other);
