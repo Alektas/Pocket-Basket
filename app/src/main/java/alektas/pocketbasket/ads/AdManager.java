33d7d1144575a04bd5740683c18b93294c35d409
Alektas <kriowolf1@gmail.com>
2020-02-25 18:57:23 +0300

    refactor: remove unused AdManager methods, accelerate 'combine' method

--- app/src/main/java/alektas/pocketbasket/ads/AdManager.java
+++ app/src/main/java/alektas/pocketbasket/ads/AdManager.java
@@ -13,9 +13,8 @@ import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.formats.NativeAdOptions;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import alektas.pocketbasket.BuildConfig;
 import alektas.pocketbasket.domain.entities.ShowcaseItemModel;
@@ -24,7 +23,6 @@ public class AdManager {
     private static final int MIN_OFFSET_OF_ADS = 10;
     private static final int NUMBER_OF_ADS = 5;
     private List<NativeAdWrapper> mNativeAds = new ArrayList<>();
-    private List<Integer> mLatestAdPositions = new ArrayList<>();
     private Context mContext;
     private String mAdId;
     private AdLoader mAdLoader;
@@ -119,87 +117,27 @@ public class AdManager {
     }
 
     private List<Object> combine(List<ShowcaseItemModel> products, List<NativeAdWrapper> ads) {
-        mLatestAdPositions.clear();
         if (ads.size() <= 0) return new ArrayList<>(products);
 
-        int totalSize = products.size() + ads.size();
-        List<Object> items = new ArrayList<>(totalSize);
+        int offset = Math.max((products.size() / ads.size() + 1), MIN_OFFSET_OF_ADS);
+        int adCount = (int) Math.ceil(products.size() / (float) offset);
+        int totalSize = products.size() + adCount;
+        Object[] array = new Object[totalSize];
 
-        int offset = Math.max((items.size() / ads.size() + 1), MIN_OFFSET_OF_ADS);
         int adPointer = 0;
         int productPointer = 0;
         int adIndex = 0;
         for (int i = 0; i < totalSize; i++) {
             if (i == adIndex && adPointer < ads.size()) {
-                items.add(ads.get(adPointer));
-                mLatestAdPositions.add(adIndex);
+                array[i] = ads.get(adPointer);
                 adPointer++;
                 adIndex += offset;
             } else if (productPointer < products.size()){
-                items.add(products.get(productPointer));
+                array[i] = products.get(productPointer);
                 productPointer++;
             }
         }
 
-        return items;
-    }
-
-    /**
-     * @return the latest loaded ads of empty list if ads haven't been loaded yet
-     */
-    public List<NativeAdWrapper> getLatestAds() {
-        return mNativeAds;
-    }
-
-    /**
-     * If {@link #combineWithLatestAds(List)} has been used then the latest positions of the ads in
-     * the combined list are known, so they are can be used for adapter notifying.
-     *
-     * @return positions of the ads in the combined list at latest combine operation.
-     * If combine operation hasn't used then returns empty list.
-     */
-    public List<Integer> getLatestAdPositions() {
-        return mLatestAdPositions;
-    }
-
-    /**
-     * If {@link #combineWithLatestAds(List)} has been used then the latest positions of the ads in
-     * the combined list are known, so they are with corresponding ads can be used to update
-     * and notify adapter.
-     *
-     * @param isWideMode state of the ads which defines size mode
-     * @return positions of the ads in the combined list at latest combine operation and
-     * corresponding ads. If combine operation hasn't used then returns empty map.
-     */
-    public Map<Integer, NativeAdWrapper> getLatestAdsWithPositions(boolean isWideMode) {
-        Map<Integer, NativeAdWrapper> map = new HashMap<>(mNativeAds.size());
-        if (mLatestAdPositions.isEmpty() || mNativeAds.isEmpty()) return map;
-
-        List<NativeAdWrapper> ads = withMode(mNativeAds, isWideMode);
-
-        for (int i = 0; i < ads.size(); i++) {
-            if (i >= mLatestAdPositions.size()) break;
-            map.put(mLatestAdPositions.get(i), ads.get(i));
-        }
-
-        return map;
-    }
-
-    /**
-     * Create copy of latest loaded ads with desired mode (wide or narrow).
-     *
-     * @param isWideMode state of the ads which defines size mode
-     * @return copy of the latest loaded ads with desired size mode
-     */
-    public List<NativeAdWrapper> withMode(boolean isWideMode) {
-        return withMode(mNativeAds, isWideMode);
-    }
-
-    private List<NativeAdWrapper> withMode(List<NativeAdWrapper> ads, boolean isWideMode) {
-        List<NativeAdWrapper> newAds = new ArrayList<>(ads.size());
-        for (NativeAdWrapper w : ads) {
-            newAds.add(new NativeAdWrapper(w.getAd(), isWideMode));
-        }
-        return newAds;
+        return Arrays.asList(array);
     }
 }
