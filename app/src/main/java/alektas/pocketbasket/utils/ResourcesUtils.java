6fa4c0a64d2501bfe0600ea8e86b49c8411fc004
Alektas <kriowolf1@gmail.com>
2020-05-10 10:41:28 +0300

    refactor: ResourcesUtils

--- app/src/main/java/alektas/pocketbasket/utils/ResourcesUtils.java
+++ app/src/main/java/alektas/pocketbasket/utils/ResourcesUtils.java
@@ -3,13 +3,14 @@ package alektas.pocketbasket.utils;
 import android.content.res.Resources;
 import android.util.Log;
 
+import androidx.annotation.AnyRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 
 import java.util.Locale;
 
 import alektas.pocketbasket.App;
 import alektas.pocketbasket.BuildConfig;
-import alektas.pocketbasket.domain.entities.ItemModel;
 
 public class ResourcesUtils {
     private static final String TAG = "ResourcesUtils";
@@ -23,37 +24,34 @@ public class ResourcesUtils {
         return BuildConfig.VERSION_NAME;
     }
 
-    public static String getString(int stringResId) {
-        return mResources.getString(stringResId);
-    }
-
     public static String getString(@NonNull String stringResName) {
         return getString(getStringId(stringResName));
     }
 
-    public static int getResId(String resName, String type) {
-        if (resName == null) return 0;
-        return mResources.getIdentifier(resName,
-                type,
-                App.getComponent().context().getPackageName());
+    public static String getString(@StringRes int stringResId) {
+        return mResources.getString(stringResId);
     }
 
     public static int getImgId(String imgResName) {
         if (imgResName == null) return 0;
-        return mResources.getIdentifier(imgResName,
-                "drawable",
-                App.getComponent().context().getPackageName());
+        return getResId(imgResName, "drawable");
     }
 
     public static int getStringId(String stringResName) {
         if (stringResName == null) return 0;
-        return mResources.getIdentifier(stringResName,
-                "string",
-                App.getComponent().context().getPackageName());
+        return getResId(stringResName, "string");
+    }
+
+    public static int getResId(@NonNull String resName, @NonNull String type) {
+        return mResources.getIdentifier(
+                resName,
+                type,
+                App.getComponent().context().getPackageName()
+        );
     }
 
 
-    public static String getResIdName(int resId) {
+    public static String getResIdName(@AnyRes int resId) {
         try {
             return mResources.getResourceEntryName(resId);
         } catch (Resources.NotFoundException e) {
@@ -62,17 +60,8 @@ public class ResourcesUtils {
         return null;
     }
 
-    public static String getDisplayName(@NonNull ItemModel item) {
-        if (item.getNameRes() == null) return item.getName();
-        return getString(item.getNameRes());
-    }
-
-    public static Locale getCurrentLocale(){
+    public static Locale getCurrentLocale() {
         return Locale.getDefault();
-    }
-
-    public static int getInDip(int dimenRes) {
-        return (int) (mResources.getDimension(dimenRes) / mResources.getDisplayMetrics().density);
     }
 
 }
